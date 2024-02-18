24934097479011b7e20d3fd687e583fe688e6f10
Michel Rosa <michel@codexa.com.br>
2024-02-18 17:30:55 -0300

    sobelow config

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule PipelineCi.MixProject do
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:excoveralls, "~> 0.10", only: :test}
+      {:excoveralls, "~> 0.10", only: :test},
+      {:sobelow, "~> 0.8", only: :dev}
     ]
   end
 
